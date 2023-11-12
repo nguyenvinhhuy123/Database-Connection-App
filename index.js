80d7a07ee6f06d5c1c621dc75810f3a5645b7da0
Hoang Tran <descience.thh10@gmail.com>
2023-11-12 14:08:15 +0700

    feat: setup 'ejs' template engine, refactor 'login' page to 'views' dir

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cfg = {
   dir: {
     root: __dirname,
     static: __dirname + "static" + sep,
+    views: __dirname + "views" + sep,
   },
 };
 console.dir(cfg, { depth: null, color: true });
@@ -17,16 +18,30 @@ console.dir(cfg, { depth: null, color: true });
 const app = express();
 app.disable("x-powered-by");
 
+app.set('view engine', 'ejs');
+app.set('views', cfg.dir.views);
 app.use(compression());
 app.use(express.static(cfg.dir.static));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// home page route
+app.get('/', (req, res) => {
+  res.render('message', { title: 'Hello World!' });
 });
 
-app.use((req, res) => {
-  res.status(404).send("Not found");
+// another route
+app.get('/hello/', (req, res) => {
+  res.render('message', { title: 'Hello again!' });
 });
+
+app.get('/login/', (req, res) => {
+    res.render('login/login');
+});
+
+// 404 errors
+app.use((req, res) => {
+  res.status(404).render('message', { title: 'Not found' });
+});
+
 
 app.listen(cfg.port, () => {
   console.log(`Server listening at http://localhost:${cfg.port}`);
