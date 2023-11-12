0cdfba4c0457edf9cfadf576c173f623026d9d3f
Hoang Tran <descience.thh10@gmail.com>
2023-11-12 13:50:29 +0700

    feat: serve static files and define working dir for es6 modules

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
 
+import { fileURLToPath } from "url";
+import { dirname, sep } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) + sep;
 const cfg = {
   port: process.env.PORT || 3000,
+  dir: {
+    root: __dirname,
+    static: __dirname + "static" + sep,
+  },
 };
+console.dir(cfg, { depth: null, color: true });
 
 const app = express();
+
+app.use(express.static(cfg.dir.static));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,3 +24,5 @@ app.get("/", (req, res) => {
 app.listen(cfg.port, () => {
   console.log(`Server listening at http://localhost:${cfg.port}`);
 });
+
+export { cfg, app };
