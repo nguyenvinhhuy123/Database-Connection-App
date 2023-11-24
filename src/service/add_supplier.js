f9ce94d81878f9ea2513167a434d307df7150559
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-11-24 14:36:40 +0700

    feat: add get_all_partner_id to later select in add_supplier

--- src/service/add_supplier.js
+++ src/service/add_supplier.js
@@ -1,5 +1,24 @@
 import database_pool from "../database_connection/database_pool.js";
 
+export async function get_all_partner_id() { 
+  try {
+    const [partner_id_list] = await database_pool.query(
+      `
+      SELECT partner_code
+      FROM partner_staff
+      `,
+      function (err, res) {
+        if (err) {
+          throw err;
+        }
+      }
+    )
+  }
+  catch { 
+    console.log(err);
+    return undefined;
+  }
+}
 export async function add_new_supplier(name, address, bank, tax, partner, phone) {
   console.log("Add start");
   try {
@@ -8,11 +27,11 @@ export async function add_new_supplier(name, address, bank, tax, partner, phone)
       var insert_supplier_sql = `
         INSERT INTO supplier
         (sup_name, address, bank_account, tax_code, partner_code)
-        = ? 
+        VALUES ? 
       `
       const [add_supplier_successful] = await database_pool.query(
         insert_supplier_sql,
-        supplier_value,
+        [[supplier_value]],
         function (err, res) {
           if (err) throw err;
           console.log("Added supplier with ID :" + res.insertId);
@@ -22,11 +41,11 @@ export async function add_new_supplier(name, address, bank, tax, partner, phone)
       var add_phone_sql = `
         INSERT INTO supplier_phone
         (sup_code, phone_number)
-        = ?
+        VALUES ?
       `
       const [add_phone_successful] = await database_pool.query(
         add_phone_sql,
-        [supplier_id, phone],
+        [[supplier_id, phone]],
         function (err, res) {
           if (err) {
             throw err;
